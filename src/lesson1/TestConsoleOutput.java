933b895ccc7b5b80a2621e7f8e8c99b3c41b4882
Viktor Savonin <viktor.savonin@meawallet.com>
2020-02-12 18:13:50 +0200

    Lesson 1

---
+++ src/lesson1/TestConsoleOutput.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class TestConsoleOutput {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("dfdsf");
+        System.out.println("dfsdfds");
+    }
+
+}
+
+
