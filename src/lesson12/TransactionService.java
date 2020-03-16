41d9d0c9f50b0c570d518f23f4d27be53e19daa9
Viktor Savonin <viktor.savonin@meawallet.com>
2020-03-16 21:14:17 +0200

    Lesson 12

--- src/lesson12/TransactionService.java
+++ src/lesson12/TransactionService.java
@@ -1,18 +1,32 @@
 package lesson12;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class TransactionService {
 
+    /*
+    1. Find all transactions in the year 2011
+        and sort them by value (small to high).
+     */
     public List<Transaction> question1(List<Transaction> transactions) {
+        List<Transaction> transactions2011 = new ArrayList<>();
+        for (Transaction transaction : transactions) {
+            if (transaction.getYear() == 2011) {
+                transactions2011.add(transaction);
+            }
+        }
 
+        TransactionYearComparator comparator = new TransactionYearComparator();
+        transactions2011.sort(comparator);
+
+        return transactions2011;
     }
 
     /*
 Questions:
 
-1. Find all transactions in the year 2011
-and sort them by value (small to high).
 
 2. What are all the unique cities where the traders work?
 3. Find all traders from Cambridge and sort them by name.
